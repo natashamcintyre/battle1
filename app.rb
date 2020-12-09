f708960e6fcd2c5a6d33ab2e1aa906b68d4b0053
Chris Whitehouse <chris_whitehouse@hotmail.com>
2020-12-09 16:30:42 +0000

    Halfway through Post / redirect / Get

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect_to '/play'
+  end
+
+  get '/play' do
+    @player_1 << session[:player_1].inspect
+    @player_2 << session[:player_2].inspect
     erb :play
   end
   # start the server if ruby file executed directly
