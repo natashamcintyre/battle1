9bb84be42c722aea0695359acbc758de2ba8a6e5
Chris Abdo <christophermabdo@icloud.com>
2020-12-10 16:11:43 +0000

    completed attacking P2

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,6 +2,6 @@ feature "attacking player 2" do
   scenario "attach player 2 and see confirmation" do
     sign_in_and_play
     click_button "Attack Player 2"
-    expect(page).to have_content "Player 2 attacked"
+    expect(page).to have_content "Chris W attacked Chris A"
   end
 end
